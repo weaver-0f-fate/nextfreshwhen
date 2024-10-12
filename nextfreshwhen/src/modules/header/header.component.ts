decf2bba153f56695bb37b6405dd1806a0e2f5f7
deniss_shutov <deniss.shutov98@gmail.com>
2024-10-12 23:34:46 +0300

    updating angular

--- nextfreshwhen/src/modules/header/header.component.ts
+++ nextfreshwhen/src/modules/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { octMarkGithub } from '@ng-icons/octicons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, NgIconComponent],
+  providers: [provideIcons({ octMarkGithub })],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
